ba9c50a9e6bcc3c20e1516d25130f38ab473c718
Eder Augusto <ederguzto@gmail.com>
2026-04-07 22:54:18 -0400

    limpando código e pastas inuteis neste repositorio

--- src/main/java/br/com/estacionamento/Main.java
+++
@@ -1,4 +0,0 @@
-package main.java.br.com.estacionamento;
-
-public class Main {
-}
--- src/main/java/br/com/lanchonete/Main.java
+++
@@ -1,7 +0,0 @@
-package main.java.br.com.lanchonete;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
